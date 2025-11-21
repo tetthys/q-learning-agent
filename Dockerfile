65fc60999bc51d07b84168acae177633009a441d
= <=>
2025-11-21 16:44:42 +0900

    set up testing env

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,4 @@ COPY requirements.txt /tmp/requirements.txt
 RUN pip install --no-cache-dir -r /tmp/requirements.txt
 
 WORKDIR /app
+ENV PYTHONPATH=/app
---
+++ src/contracts/__init__.py
@@ -0,0 +1,13 @@
+# src/contracts/__init__.py
+
+from .Action import Action
+from .Agent import Agent
+from .QTable import QTable
+from .Reward import Reward
+
+__all__ = [
+    "Action",
+    "Agent",
+    "QTable",
+    "Reward",
+]
